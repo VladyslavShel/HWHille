b6090117873c113889aa07ee0a9d089995030b39
VladyslavShel <80919922+VladyslavShel@users.noreply.github.com>
2021-03-29 14:44:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int First=6;
+        int Second = 2;
+        int Average = 0;
+        Average = (First + Second)/2;
+	System.out.println("Среднее арифметическое двух чисел=" + Average);
+    }
+}
